6abcb6e198cec8e14623e076aba38e191a62a1a9
Gustavo de Miranda <gustavolpu@hotmail.com>
2020-06-02 20:43:55 -0300

    Added Local Notifications.

--- AmazingNotesApp/AmazingNotesApp/Views/AddItemPage.xaml.cs
+++ AmazingNotesApp/AmazingNotesApp/Views/AddItemPage.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using AmazingNotesApp.Models;
+using Plugin.LocalNotifications;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,14 +17,43 @@ namespace AmazingNotesApp.Views
         public AddItemPage()
         {
             InitializeComponent();
+            SwitchReminder.Toggled += EnableDisableReminder;
+        }
+
+        private void EnableDisableReminder(object sender, EventArgs e)
+        {
+            DatePickerReminder.IsEnabled = SwitchReminder.IsToggled;
+            TimePickerReminder.IsEnabled = SwitchReminder.IsToggled;
         }
 
         private void ButtonAddItem_Clicked(object sender, EventArgs e)
         {
+
+            //Create Item
             var item = new Item();
             item.Title = EntryTitle.Text;
             item.Description = EntryDescription.Text;
             App.Items.Add(item);
+
+            //LocalNotification
+            if (SwitchReminder.IsToggled)
+            {
+                var reminderDateTime = new DateTime(
+                    DatePickerReminder.Date.Year,
+                    DatePickerReminder.Date.Month,
+                    DatePickerReminder.Date.Day,
+                    TimePickerReminder.Time.Hours,
+                    TimePickerReminder.Time.Minutes,
+                    0
+                    );
+
+                CrossLocalNotifications.Current.Show(
+                    item.Title,
+                    item.Description,
+                    App.Items.Count - 1,
+                    reminderDateTime
+                    );
+            }
 
             Navigation.PopModalAsync(true);
         }
